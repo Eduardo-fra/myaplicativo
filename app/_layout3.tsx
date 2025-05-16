e1346041ef4bb68b2175d22e7aee79b462b61c2d
eduardo francisco <eduardo.francisco16@etec.sp.gov.br>
2025-05-16 17:23:02 -0300

    alteração 16/05

---
+++ app/_layout3.tsx
@@ -0,0 +1,10 @@
+import { useRouter } from "expo-router";
+import React from "react";
+import { Button, View } from "react-native";
+
+export default function HomeScreen() {
+return(
+    <View></View>
+);
+
+}
